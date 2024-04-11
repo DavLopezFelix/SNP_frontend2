871b5447bbbb94037291b3a4ee2cc623e4daa52a
David Lopez Felix <dlopezf.19@gmail.com>
2024-04-11 02:23:25 -0500

    test env var ok

--- src/nortecentrosesionsnp/descargas.js
+++ src/nortecentrosesionsnp/descargas.js
@@ -14,7 +14,7 @@ function Descargas() {
   const [error, setError] = useState('');
   const [confirmationMessage, setConfirmationMessage] = useState('');
   const [showPopup, setShowPopup] = useState(false); // Estado para controlar la visibilidad del mensaje emergente
-  const apiKey = 'GafXD93ZXV3jbslFcBaXT1ALLcKkBBG04JP9ZmCO';
+  const apiKey = process.env.REACT_APP_API_KEY;
 
   useEffect(() => {
     const fetchData = async () => {
